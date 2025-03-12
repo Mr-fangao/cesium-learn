127bb7f26a1f8c64610bb3a58d98dee1b52df8b8
Mr-fangao <Mr.undefine@protonmail.com>
2025-03-12 08:47:46 +0800

    dat.gui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 /*
  * @Author: liqifeng
  * @Date: 2025-03-11 11:00:44
- * @LastEditors: liqifeng Mr.undefine@protonmail.com
- * @LastEditTime: 2025-03-11 11:07:35
+ * @LastEditors: Mr-fangao Mr.undefine@protonmail.com
+ * @LastEditTime: 2025-03-11 22:12:30
  * @Description: 
  */
 import { createApp } from 'vue';
@@ -12,6 +12,7 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import './style.css'
 import router from "./router";
+import * as dat from 'dat.gui';
 // 创建应用实例
 const app = createApp(App);
 
@@ -22,6 +23,7 @@ app.use(pinia);
 // 使用 Element Plus
 app.use(ElementPlus);
 
+app.config.globalProperties.$dat = dat;
 app.use(router);
 // 挂载应用
 app.mount('#app');
--- src/views/cesiumviews/components/shader.vue
+++ src/views/cesiumviews/components/shader.vue
@@ -1,23 +1,41 @@
 <!--
  * @Author: liqifeng
  * @Date: 2025-03-11 16:59:45
- * @LastEditors: liqifeng Mr.undefine@protonmail.com
- * @LastEditTime: 2025-03-11 17:04:44
+ * @LastEditors: Mr-fangao Mr.undefine@protonmail.com
+ * @LastEditTime: 2025-03-11 22:18:21
  * @Description: 
 -->
 <script setup>
-import { ref } from 'vue'
+// 获取全局挂载的 dat.GUI
+const { proxy } = getCurrentInstance();
+const dat = proxy.$dat;
+const gui = new dat.GUI({ autoPlace: false })
+// 定义需要调整的参数
+const params = {
+  speed: 0.5,
+  color: '#ff0000',
+  visible: true,
+};
 
-defineProps({
-  msg: String,
-})
-
+// 添加控制器
+gui.add(params, 'speed', 0, 1).name('Speed').onChange((value) => {
+  console.log('Speed changed to:', value);
+});;
+gui.addColor(params, 'color').name('Color');
+gui.add(params, 'visible').name('Visible');
+const guiContainer = ref(null);
+onMounted(() => {
+  guiContainer.value.appendChild(gui.domElement);
+});
+onBeforeUnmount(() => {
+  gui.destroy();
+});
 const count = ref(0)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
+  <div ref="guiContainer"></div>
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
@@ -28,17 +46,12 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
+    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank">Vue Docs Scaling up Guide</a>.
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
